3906f21cdd966c76be7e2e3d727758201f38d4d3
Bruno Holanda <br.holanda15@gmail.com>
2024-11-18 08:15:52 -0300

    feat: start game

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,13 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
 
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    "PC"
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
   end
 end
