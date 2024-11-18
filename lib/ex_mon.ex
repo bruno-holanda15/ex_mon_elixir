0b1c369f174687ffce4e5cae6e407954970f6b6d
Bruno Holanda <br.holanda15@gmail.com>
2024-11-18 08:25:11 -0300

    feat: show infos

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,13 +1,18 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
+
+  @computer_name "Megatron"
 
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
   end
 
   def start_game(player) do
-    "PC"
+    @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
